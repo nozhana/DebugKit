19b5b82294d43b939a6f874905a48cc5904090c2
Nozhan A. <nozhana@gmail.com>
2025-11-06 17:01:59 +0330

    fixups

--- Sources/DebugKit/Networking/URLSession+Debug.swift
+++ Sources/DebugKit/Networking/URLSession+Debug.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 extension URLSession {
-    /// Shared debugging ``Foundation/URLSession`` object.
+    /// Shared debugging `URLSession` object.
     ///
     /// All requests made using this instance will be monitored in the Debug Menu.
     ///
-    /// If you need to create a custom ``Foundation/URLSession`` instance, use the ``Foundation/URLSessionConfiguration/debug`` configuration like so:
+    /// If you need to create a custom `URLSession` instance, use the ``Foundation/URLSessionConfiguration/debug`` configuration like so:
     /// ```swift
     /// // Simple instance
     /// let session = URLSession(configuration: .debug)
@@ -31,11 +31,11 @@ extension URLSession {
 }
 
 extension URLSessionConfiguration {
-    /// Shared debugging ``Foundation/URLSessionConfiguration`` object.
+    /// Shared debugging `URLSessionConfiguration` object.
     ///
     /// All sessions created using this configuration will be monitored in the Debug Menu.
     ///
-    /// If you don't need to create a separate ``Foundation/URLSession`` instance, check out the shared ``Foundation/URLSession/debug`` session.
+    /// If you don't need to create a separate `URLSession` instance, check out the shared ``Foundation/URLSession/debug`` session.
     ///
     /// - SeeAlso: `URLSession.`‌``Foundation/URLSession/debug``
     public static let debug = {
