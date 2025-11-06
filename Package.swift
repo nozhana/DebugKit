1618a877ab6b05e8438629051df93256c9f78a1e
Nozhan A. <nozhana@gmail.com>
2025-11-06 17:56:11 +0330

    docc plugin

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
             name: "DebugKit",
             targets: ["DebugKit"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+    ],
     targets: [
         .target(
             name: "DebugKit"),
