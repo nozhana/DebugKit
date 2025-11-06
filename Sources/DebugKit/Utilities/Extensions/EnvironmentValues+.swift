37f149ee32f74544220f81512c9d7c013865b2d5
Nozhan A. <nozhana@gmail.com>
2025-11-06 16:58:14 +0330

    move view extensions

--- Sources/DebugKit/Utilities/Extensions/EnvironmentValues+.swift
+++ Sources/DebugKit/Utilities/Extensions/EnvironmentValues+.swift
@@ -9,7 +9,7 @@ import Combine
 import SwiftUI
 
 extension EnvironmentValues {
-    /// A publisher that publishes ``DebugMenuMessage`` items produced by a ``PostMessageCallback`` in a ``Content`` block.
+    /// A publisher that publishes ``DebugMenuMessage`` items produced by a ``DebugMenuView/PostMessageCallback`` in a ``DebugMenuView/Content`` block.
     ///
     /// ## Usage
     /// Subscribe to this publisher to receive debug menu messages.
@@ -30,42 +30,12 @@ extension EnvironmentValues {
     /// }
     /// ```
     ///
-    /// - Note: This publisher is canonically equivalent to [`DebugMenuView.messagePublisher`](DebugMenuView/messagePublisher).
+    /// - Note: This publisher is canonically equivalent to ``DebugMenuView/messagePublisher``.
     ///
     /// - SeeAlso: ``DebugMenuView/messagePublisher``, ``DebugMenuMessage``, ``DebugMenuView/PostMessageCallback``, ``DebugMenuView/Content``
     public var debugMenuMessage: some Publisher<DebugMenuMessage, Never> {
         NotificationCenter.default.publisher(for: .debugMenuMessage)
             .receive(on: RunLoop.main)
             .compactMap({ $0.userInfo?["message"] as? DebugMenuMessage })
-    }
-}
-
-extension View {
-    /// Add an observer to the view hierarchy to perform an action when a debug menu message is received.
-    /// - Parameters:
-    ///   - messages: A variadic array of debug menu messages to observe. If left empty, all messages will be observed.
-    ///   - action: The action to perform when a message is received.
-    /// - Returns: The observing view hierarchy.
-    /// - SeeAlso: ``onDebugMenuMessage(_:perform:)``
-    public func onDebugMenuMessage(_ messages: DebugMenuMessage..., perform action: @escaping (_ message: DebugMenuMessage) -> Void) -> some View {
-        onReceive(\.debugMenuMessage) {
-            if messages.isEmpty || messages.contains($0) {
-                action($0)
-            }
-        }
-    }
-    
-    /// Add an observer to the view hierarchy to perform an action when a debug menu message is received.
-    /// - Parameters:
-    ///   - messages: A variadic array of debug menu messages to observe. If left empty, all messages will be observed.
-    ///   - action: The action to perform when a message is received.
-    /// - Returns: The observing view hierarchy.
-    /// - SeeAlso: ``onDebugMenuMessage(_:perform:)-3qicl``
-    public func onDebugMenuMessage(_ messages: DebugMenuMessage..., perform action: @escaping () -> Void) -> some View {
-        onReceive(\.debugMenuMessage) {
-            if messages.isEmpty || messages.contains($0) {
-                action()
-            }
-        }
     }
 }
--- Sources/DebugKit/Utilities/Extensions/View+.swift
+++ Sources/DebugKit/Utilities/Extensions/View+.swift
@@ -8,6 +8,36 @@
 import SwiftUI
 
 extension View {
+    /// Add an observer to the view hierarchy to perform an action when a debug menu message is received.
+    /// - Parameters:
+    ///   - messages: A variadic array of debug menu messages to observe. If left empty, all messages will be observed.
+    ///   - action: The action to perform when a message is received.
+    /// - Returns: The observing view hierarchy.
+    /// - SeeAlso: ``onDebugMenuMessage(_:perform:)``
+    public func onDebugMenuMessage(_ messages: DebugMenuMessage..., perform action: @escaping (_ message: DebugMenuMessage) -> Void) -> some View {
+        onReceive(\.debugMenuMessage) {
+            if messages.isEmpty || messages.contains($0) {
+                action($0)
+            }
+        }
+    }
+    
+    /// Add an observer to the view hierarchy to perform an action when a debug menu message is received.
+    /// - Parameters:
+    ///   - messages: A variadic array of debug menu messages to observe. If left empty, all messages will be observed.
+    ///   - action: The action to perform when a message is received.
+    /// - Returns: The observing view hierarchy.
+    /// - SeeAlso: ``onDebugMenuMessage(_:perform:)-3qicl``
+    public func onDebugMenuMessage(_ messages: DebugMenuMessage..., perform action: @escaping () -> Void) -> some View {
+        onReceive(\.debugMenuMessage) {
+            if messages.isEmpty || messages.contains($0) {
+                action()
+            }
+        }
+    }
+}
+
+extension View {
     @ViewBuilder
     func `if`(_ condition: Bool, @ViewBuilder content: @escaping (Self) -> some View) -> some View {
         if condition {
